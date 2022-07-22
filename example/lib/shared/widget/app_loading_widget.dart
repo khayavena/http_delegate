e604835253921c6ccbea18531db58e1787bdeb86
Khaya Verna <khayav@touchsides.com>
2022-07-22 17:26:48 +0200

    Refactor and update files

--- example/lib/shared/widget/app_loading_widget.dart
+++ example/lib/shared/widget/app_loading_widget.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:http_example/shared/widget/animated_dot_widget.dart';
 import 'package:http_example/shared/widget/dot_type.dart';
 
 class AppLoadingWidget extends StatefulWidget {
@@ -50,38 +49,29 @@ class _AppLoadingWidgetState extends State<AppLoadingWidget>
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             translate(
-              child: Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: Dot(
-                  radius: 10.0,
-                  color: widget.dotOneColor,
-                  type: widget.dotType,
-                  icon: widget.dotIcon,
-                ),
+              child: AnimatedDotWidget(
+                radius: 10.0,
+                color: widget.dotOneColor,
+                type: widget.dotType,
+                icon: widget.dotIcon,
               ),
               animation: animation_1,
             ),
             translate(
-              child: Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: Dot(
-                  radius: 10.0,
-                  color: widget.dotTwoColor,
-                  type: widget.dotType,
-                  icon: widget.dotIcon,
-                ),
+              child: AnimatedDotWidget(
+                radius: 10.0,
+                color: widget.dotTwoColor,
+                type: widget.dotType,
+                icon: widget.dotIcon,
               ),
               animation: animation_2,
             ),
             translate(
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Dot(
-                    radius: 10.0,
-                    color: widget.dotThreeColor,
-                    type: widget.dotType,
-                    icon: widget.dotIcon,
-                  ),
+                child: AnimatedDotWidget(
+                  radius: 10.0,
+                  color: widget.dotThreeColor,
+                  type: widget.dotType,
+                  icon: widget.dotIcon,
                 ),
                 animation: animation_3),
           ],
@@ -97,7 +87,10 @@ class _AppLoadingWidgetState extends State<AppLoadingWidget>
         0.0,
         _calculateAnim1DYOffset(animation),
       ),
-      child: child,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: child,
+      ),
     );
   }
 
@@ -135,50 +128,5 @@ class _AppLoadingWidgetState extends State<AppLoadingWidget>
     });
 
     controller.repeat();
-  }
-}
-
-class Dot extends StatelessWidget {
-  final double radius;
-  final Color color;
-  final DotType type;
-  final Icon icon;
-
-  const Dot(
-      {Key? key,
-      required this.radius,
-      required this.color,
-      required this.type,
-      required this.icon})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: type == DotType.icon ? _buildIcon() : _buildAnimatedDot(),
-    );
-  }
-
-  Widget _buildIcon() {
-    return Icon(
-      icon.icon,
-      color: color,
-      size: 1.3 * radius,
-    );
-  }
-
-  Widget _buildAnimatedDot() {
-    return Transform.rotate(
-      angle: type == DotType.diamond ? pi / 4 : 0.0,
-      child: Container(
-        width: radius,
-        height: radius,
-        decoration: BoxDecoration(color: color, shape: _buildShape()),
-      ),
-    );
-  }
-
-  BoxShape _buildShape() {
-    return type == DotType.circle ? BoxShape.circle : BoxShape.rectangle;
   }
 }
